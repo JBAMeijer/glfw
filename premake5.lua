e28ca586f2ad87280430c4d590f8a38901953ec7
Joey Meijer <meijerjoey@gmail.com>
2020-11-21 22:19:45 +0100

    Changes to lua file

--- premake5.lua
+++ premake5.lua
@@ -1,43 +1,24 @@
 project "GLFW"
     kind "StaticLib"
-    kind "StaticLib"
     language "C"
-    language "C"
-
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-
-	files
     files
-	{
     {
         "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
-        "src/glfw_config.h",
-        "src/context.c",
         "src/context.c",
         "src/init.c",
-        "src/init.c",
-        "src/input.c",
         "src/input.c",
         "src/monitor.c",
-        "src/monitor.c",
-        "src/vulkan.c",
         "src/vulkan.c",
         "src/window.c"
-        "src/window.c"
-    }
     }
 
     filter "system:linux"
-
         pic "On"
 
         systemversion "latest"
@@ -63,53 +44,33 @@ project "GLFW"
         }
 
 
-	filter "system:windows"
     filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
-        buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "10.0.17134.0"
         systemversion "latest"
         staticruntime "On"
-        staticruntime "On"
-
 
         files
-        files
-        {
         {
             "src/win32_init.c",
-            "src/win32_init.c",
-            "src/win32_joystick.c",
             "src/win32_joystick.c",
             "src/win32_monitor.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
             "src/win32_time.c",
             "src/win32_thread.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
             "src/win32_window.c",
             "src/wgl_context.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
             "src/egl_context.c",
             "src/osmesa_context.c"
-            "src/osmesa_context.c"
-        }
         }
 
-
-		defines 
         defines 
 		{ 
-        { 
             "_GLFW_WIN32",
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
             "_CRT_SECURE_NO_WARNINGS"
 		}
-        }
-    filter { "system:windows", "configurations:Release" }
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
-        buildoptions "/MT"
+    
+   filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
